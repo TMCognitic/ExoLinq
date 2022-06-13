608363d2c3c5c7e2b7ec0b2973b02c2b3bc95eae
Morre Thierry <thierry.morre@cognitic.be>
2022-06-13 15:25:03 +0200

    Ajoutez des fichiers projet.

---
+++ ExoLinq/Program.cs
@@ -0,0 +1,17 @@
+﻿using ExoLinq.Context;
+
+namespace ExoLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataContext dataContext = new DataContext();
+
+            foreach(Student student in dataContext.Students)
+            {
+                Console.WriteLine($"{student.Last_Name} {student.First_Name}");
+            }
+        }
+    }
+}
